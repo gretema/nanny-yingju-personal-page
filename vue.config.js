dbd4ade50214941a06f21a9d8ca90bcf34492e1a
gretema <jeanma1995@gmail.com>
2020-03-08 14:00:59 +0800

    修改 vue.config.js 預渲染程式碼

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,22 @@
-var path = require('path')
-var PrerenderSpaPlugin = require('prerender-spa-plugin')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
+const path = require('path')
 
 // 預渲染
 module.exports = {
-  // ...
-  plugins: [
-    new PrerenderSpaPlugin(
-      // 編譯後的 html 需要存放的路径
-      path.join(__dirname, '../dist'),
-      // 列出哪些路由需要預渲染
-      ['/', '/about', '/contact']
-    )
-  ]
+  productionSourceMap: false,
+  configureWebpack(config) {
+    if ('production' === process.env.NODE_ENV) {
+      config.plugins.push(
+        new PrerenderSPAPlugin({
+          staticDir: path.join(__dirname, 'dist'),
+          routes: ['/'],
+          renderer: new Renderer({
+            headless: false,
+            renderAfterTime: 5000
+          })
+        })
+      )
+    }
+  }
 }
