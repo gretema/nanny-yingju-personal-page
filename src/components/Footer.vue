ac268427cbdec0290e97026ff29a9c9f5aa280b2
gretema <jeanma1995@gmail.com>
2020-03-09 22:01:54 +0800

    changed footer FB fanpage layout

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,26 +3,37 @@
     <footer class="py-3 px-3 bg-primary">
       <div class="container">
         <div class="row">
-          <div class="col-md-6">
-            <h2 class="mb-md-5 mb-3">保母乾媽的育花園</h2>
-            <p class="footer-idea">
-              從事托育工作二十年來，
-              秉持著創新托育的精神，
-              以開闊的視野
-              提供不同於傳統刻板印象的
-              居家托育服務。
+          <div class="col-md-4">
+            <h2 class="mb-3">保母乾媽的育花園</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4 d-md-flex align-items-md-center">
+            <p class="footer-idea mb-0">
+              從事托育工作二十年來，秉持著創新托育的精神，以開闊的視野提供不同於傳統刻板印象的居家托育服務。
             </p>
           </div>
-          <div class="col-md-6 d-md-flex flex-md-column align-items-center">
-            <div class="contact mt-3 mt-md-0 mb-2">
+          <div class="col-md-4 d-md-flex flex-md-column justify-content-center">
+            <div class="contact mt-3 mt-md-0">
               <p class="mb-0">
                 <i class="fas fa-phone-alt mr-2"></i>0986-698-675
               </p>
               <p class="mb-0">
                 <i class="fas fa-map-marker-alt mr-2"></i>台中市西屯區，近市警局（舊）
               </p>
+              <p class="mb-0">
+                <i class="fas fa-clock mr-2"></i>週一至週五 8:00 - 17:30
+              </p>
             </div>
-            <div class="fb-page" data-href="https://www.facebook.com/%E4%BF%9D%E6%AF%8D%E4%B9%BE%E5%AA%BD%E7%9A%84%E8%82%B2%E8%8A%B1%E5%9C%92-110085666999240/" data-tabs="" data-width="" data-height="" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/%E4%BF%9D%E6%AF%8D%E4%B9%BE%E5%AA%BD%E7%9A%84%E8%82%B2%E8%8A%B1%E5%9C%92-110085666999240/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/%E4%BF%9D%E6%AF%8D%E4%B9%BE%E5%AA%BD%E7%9A%84%E8%82%B2%E8%8A%B1%E5%9C%92-110085666999240/">保母乾媽的育花園</a></blockquote></div>
+          </div>
+          <div class="col-md-4 mt-3 mt-md-0">
+            <div
+                class="fb-page"
+                data-href="https://www.facebook.com/%E4%BF%9D%E6%AF%8D%E4%B9%BE%E5%AA%BD%E7%9A%84%E8%82%B2%E8%8A%B1%E5%9C%92-110085666999240/"
+                data-width="380"
+                data-hide-cover="false"
+                data-show-facepile="false"
+              ></div>
           </div>
         </div>
       </div>
