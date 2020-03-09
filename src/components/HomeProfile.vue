23c9b07f3bb6565db9f7a0badf28fd8be2c4d644
gretema <jeanma1995@gmail.com>
2020-03-09 23:46:59 +0800

    optimized home profile

--- src/components/HomeProfile.vue
+++ src/components/HomeProfile.vue
@@ -1,23 +1,44 @@
 <template>
   <div>
-    <section class="container-fluid py-4 bg-light" style="position: relative;">
-      <div class="row">
-        <!--左側滿版圖片-->
-        <div class="col-md-5 home-avatar"></div>
-        <!--右側文字敘述，響應式文字顏色要在 .row 調整-->
-        <div class="container">
+    <section class="container-fluid bg-light">
+      <!--桌機版-->
+      <div class="container py-3 d-none d-md-block">
+        <div class="row d-flex align-items-center justify-content-around">
+        <!--桌機版左側照片-->
+        <div class="col-md-3 home-avatar"></div>
+        <!--桌機版簡介與按鈕-->
+        <div class="col-md-7 py-3">
+          <h3>張瑛如</h3>
+          <p>
+            對托兒視如己出，因此自稱為孩子們的「乾媽」。
+            二十餘年專業的居家育兒經驗中，總是持續設計創意活動來引領孩子們學習成長；加上開放、彈性、樂於溝通的特質，相信能帶給您滿意又放心的托育服務。
+          </p>
+          <a href="#" class="btn btn-outline-info">了解更多</a>
+        </div>
+      </div>
+      </div>
+      <!--手機版-->
+      <div class="d-md-none container py-3">
+          <!--行動版姓名與照片-->
+          <div class="row d-flex align-items-center mb-2">
+            <div class="col">
+              <h2>張瑛如</h2>
+            </div>
+            <div class="col d-flex justify-content-end">
+              <div class="mobile-home-avatar"></div>
+            </div>
+          </div>
+          <!--行動版簡介與按鈕-->
           <div class="row justify-content-end text-dark">
-            <div class="col-md-7">
-              <h3>張瑛如</h3>
+            <div class="col d-md-none">
               <p>
                 對托兒視如己出，因此自稱為孩子們的「乾媽」。
-                二十餘年專業的居家育兒經驗中，總是持續設計創意活動來引領孩子們學習成長；加上創新、彈性、樂於溝通的特質，相信能帶給您滿意又放心的托育服務。
+                二十餘年專業的居家育兒經驗中，總是持續設計創意活動來引領孩子們學習成長；加上開放、彈性、樂於溝通的特質，相信能帶給您滿意又放心的托育服務。
               </p>
-              <a href="#" class="btn btn-outline-info">了解更多</a>
+              <a href="#" class="btn btn-info">了解更多</a>
             </div>
           </div>
         </div>
-      </div>
     </section>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <HomeHeader/>
+    <WorriesSection/>
     <HomeProfile/>
   </div>
 </template>
@@ -9,12 +10,14 @@
 // @ is an alias to /src
 import HomeHeader from '@/components/HomeHeader.vue';
 import HomeProfile from '@/components/HomeProfile.vue';
+import WorriesSection from '@/components/Worries.vue';
 
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeProfile,
+    WorriesSection,
   },
   data() {
     return {
