c01f1f5ca94a54d817ef9743b684cbea1ecbd574
gretema <jeanma1995@gmail.com>
2020-03-11 01:13:52 +0800

    關於頁桌機版 Layout 完成

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,7 +18,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav ml-auto">
-            <router-link class="nav-item nav-link" to="/">首頁</router-link>
+            <router-link class="nav-item nav-link" to="/" exact>首頁</router-link>
             <router-link class="nav-item nav-link" to="/about">關於</router-link>
             <router-link class="nav-item nav-link" to="/feature">特色</router-link>
             <router-link class="nav-item nav-link" to="/faq">常見問題</router-link>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,98 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="jumbotron jumbotron-fluid mb-0 about-header d-flex align-items-center">
+      <div class="container d-flex justify-content-center">
+        <h2 class="display-6 p-3 m-0 text-center">
+          <span class="d-block d-md-inline">關於張瑛如，</span>
+          <span class="d-block d-md-inline">我還想知道......</span>
+        </h2>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <!--靠行的身份-->
+      <div class="row py-3 bg-light">
+        <div class="col-md-4 text-center">
+          <div style="font-size: 3rem;">
+            <i class="fas fa-dollar-sign text-info"></i>
+          </div>
+          <h3>托育補助？
+          </h3>
+          <p>政府認證之專業保母，家長可申請托育補助。</p>
+        </div>
+        <div class="col-md-4 text-center">
+          <div style="font-size: 3rem;">
+            <i class="fas fa-address-card text-info"></i>
+          </div>
+          <h3>受政府管理？</h3>
+          <p>社區保母系統第二區文心教保小組成員。</p>
+        </div>
+        <div class="col-md-4 text-center">
+          <div style="font-size: 3rem;">
+            <i class="fas fa-user-tie text-info"></i>
+          </div>
+          <h3>專業年資？</h3>
+          <p>1997 年迄今專業居家托育資歷。</p>
+        </div>
+      </div>
+    </div>
+    <div class="container mb-5">
+      <!--經歷時間軸-->
+      <div class="row">
+        <div class="col">
+          <h2 class="text-center my-3 font-weight-bold experience-title">特殊經歷</h2>
+        </div>
+      </div>
+      <div class="timeline-section">
+        <div class="row my-3 timeline-pointer">
+          <div class="col-sm-6">
+            <h4 class="text-center bg-primary rounded-pill py-2">2019.06 居家托育博覽會 講師</h4>
+            <p>於台中市第二區居家托育服務中心舉辦的居家托育博覽會中，擔任講師，向有意進入保母業界的人士分享托育經驗。</p>
+          </div>
+        </div>
+        <div class="row my-3 flex-sm-row-reverse timeline-pointer">
+          <div class="col-sm-6">
+            <h4 class="text-center bg-primary rounded-pill py-2">2019.01 社區繪本閱讀活動 發起人</h4>
+            <p>號召社區中其他保母於每月舉辦繪本說故事活動。</p>
+          </div>
+        </div>
+        <div class="row my-3 timeline-pointer">
+          <div class="col-sm-6">
+            <h4 class="text-center bg-primary rounded-pill py-2">2018.11 獲電視節目【今天去哪ㄦ】採訪</h4>
+            <p>
+              群健電視台受台中市政府新聞局委託，針對 2018 年度地方文化進行自製節目採訪，製作團隊造訪保母乾媽家中進行「一日把拔」體驗（
+              <a
+                href="https://www.tbc.net.tw/News/NewsDetail?id=b1eefa66-bbff-47ec-a1f7-93a705e217c2&newsType=5"
+                class="text-info"
+              >影片</a> 14:54 開始）。
+            </p>
+          </div>
+        </div>
+        <div class="row my-3 flex-sm-row-reverse timeline-pointer">
+          <div class="col-sm-6">
+            <h4 class="text-center bg-primary rounded-pill py-2">2016 二度榮獲台中市政府「優質托育人員」表揚</h4>
+          </div>
+        </div>
+        <div class="row my-3 timeline-pointer">
+          <div class="col-sm-6">
+            <h4 class="text-center bg-primary rounded-pill py-2">2008 中台科技大學幼兒保育學系畢業</h4>
+          </div>
+        </div>
+        <div class="row my-3 flex-sm-row-reverse timeline-pointer">
+          <div class="col-sm-6">
+            <h4 class="text-center bg-primary rounded-pill py-2">2004 榮獲台中市政府「優質托育人員」表揚</h4>
+          </div>
+        </div>
+        <div class="row my-3 timeline-pointer">
+          <div class="col-sm-6">
+            <h4 class="text-center bg-primary rounded-pill py-2">2003 榮任台中市家扶中心保母聯誼會副會長</h4>
+          </div>
+        </div>
+        <div class="row my-3 flex-sm-row-reverse timeline-pointer">
+          <div class="col-sm-6">
+            <h4 class="text-center bg-primary rounded-pill py-2">1998 考取保母技術士證照</h4>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
